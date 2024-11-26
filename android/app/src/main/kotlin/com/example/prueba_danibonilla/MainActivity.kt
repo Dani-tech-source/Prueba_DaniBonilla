a6449c017e7b684c8b8cc6fa38f122539acefc63
Dani <bonilladani036@gmail.com>
2024-11-25 21:00:33 -0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/prueba_danibonilla/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.prueba_danibonilla
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'package:prueba_danibonilla/firebase_options.dart';
+import 'package:prueba_danibonilla/src/router.dart';
+
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      routerConfig: router
+     
+
+    );
+  }
+}
